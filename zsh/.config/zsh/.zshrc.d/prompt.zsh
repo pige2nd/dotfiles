28694d0212b1c2252fce7b1833df14954b9d883e
pige2nd <512357024@qq.com>
2026-07-24 23:12:18 +0800

    Refine Starship prompt spacing

--- zsh/.config/zsh/.zshrc.d/prompt.zsh
+++ zsh/.config/zsh/.zshrc.d/prompt.zsh
@@ -6,3 +6,18 @@
 # 避免 zsh 为右侧填充额外空格，保证 $fill 对齐准确。
 export ZLE_RPROMPT_INDENT=0
 eval "$(starship init zsh)"
+
+# 跳过新 shell 的首个提示符，之后每次显示提示符前插入空行。
+# 因此执行命令和空回车都会分隔，但新终端顶部不会留白。
+autoload -Uz add-zsh-hook
+typeset -gi _xx_prompt_seen=0
+
+_xx_prompt_separate_output() {
+  if (( _xx_prompt_seen )); then
+    print
+  else
+    _xx_prompt_seen=1
+  fi
+}
+
+add-zsh-hook precmd _xx_prompt_separate_output
